2b7c664050afea87855b80fe28f2e5cf9fa99afe
Paras Puru <paraskmahto@gmail.com>
2024-01-04 23:03:47 +0530

    changes on db connection

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const userRouter = require("./routes/user-route");
-const blogRouter = require("./routes/blog-route");
+const express = require('express');
+const mongoose = require('mongoose');
+const userRouter = require('./routes/user-route');
+const blogRouter = require('./routes/blog-route');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -10,22 +10,17 @@ app.use(express.json());
 app.use("/v1/api/user", userRouter);
 app.use("/v1/api/blog", blogRouter);
 
-mongoose
-  .connect(
-    "mongodb+srv://paraspuru143:RIpWKDfzfnqOuq0n@parascluster.q5hebjq.mongodb.net/Blog?retryWrites=true&w=majority"
-  )
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`The server is running on localhost:${PORT}`);
-    });
-  })
-  .then(() => {
-    console.log(`DB connected successfully!!`);
-  })
-  .catch((error) => {
+mongoose.connect("mongodb+srv://paraspuru143:RIpWKDfzfnqOuq0n@parascluster.q5hebjq.mongodb.net/Blog?retryWrites=true&w=majority").then(() => {
+    app.listen(PORT, ()=> {
+        console.log(`The server is running on localhost:${PORT}`)
+    })
+}).then(() => {
+    console.log(`DB connected successfully!!`)
+}).catch((error) => {
     console.log(`Error in DB while connecting`);
-  });
+})
 
 app.use("/", (req, res, next) => {
-  res.send("Hello World");
-});
+    res.send("Hello World");
+})
+
